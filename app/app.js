2080ce1c09c805189324825dc2fad6d74ec005f6
Soumya Ranjan Mohanty <smohanty.dev@gmail.com>
2016-02-14 01:28:38 +0530

    state manager default view

--- app/app.js
+++ app/app.js
@@ -4,5 +4,15 @@ angular.module("myApp", ["ui.router"])
             url: "/home",
             templateUrl: "templates/home.html"
         }) 
+        .state("contact",{
+            url: "/contact",
+            template: "<h1>Contact</h1><p>Ui Router Contact</p>"
+        }) 
+        .state("about",{
+            url: "/about?view",
+            templateUrl: function ($stateParams){
+                return 'templates/' + $stateParams.view + '.html';
+            }
+        }) 
         
 });
