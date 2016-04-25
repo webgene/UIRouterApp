44cef301db00c9af12ec41adb19f05b89729a47a
Soumya Ranjan Mohanty <smohanty.dev@gmail.com>
2016-04-25 18:57:10 +0530

    State Helpers

--- app/nested-state-views/app.js
+++ app/nested-state-views/app.js
@@ -42,14 +42,32 @@ angular.module("nestedState", ["ui.router"])
     });
 
 
-angular.module("stateHelper", ["ui.router"])
-    .config(function($stateProvider){
-        $stateProvider
-            .state("parent",{
-                url: "/parent",
-                template: "<h1>Parent</h1><p>Parent state</p><div ui-view></div>"
-            })
-
-
-
+angular.module('stateHelper', ['ui.router', 'ui.router.stateHelper'])
+    .config(function(stateHelperProvider){
+        stateHelperProvider.state({
+            name: 'parent',
+            templateUrl: 'templates/parent.html',
+            children: [
+                {
+                    name: 'child1',
+                    templateUrl: 'templates/child1.html',
+                    children: [
+                        {
+                            name: 'child11',
+                            template: '<h1>Child11</h1><p>This is the Child11 state, Parent State is Child1</p>'
+                        }
+                    ]
+                },
+                {
+                    name: 'child2',
+                    template: '<h1>Child2</h1><p>This is Child2 state</p><div ui-view></div>',
+                    children: [
+                        {
+                            name: 'child21',
+                            template: '<h1>Child21</h1><p>This is the Child21 state, Parent State is Child2</p>'
+                        }
+                    ]
+                }
+            ]
+        }, { keepOriginalNames: true });
     });
