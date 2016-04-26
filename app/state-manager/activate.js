13b1f9050eee59e192207f7c0c11a98bce5cbc3f
Soumya Ranjan Mohanty <smohanty.dev@gmail.com>
2016-04-26 20:11:41 +0530

    Refractored Activate

--- app/state-manager/activate.js
+++ app/state-manager/activate.js
@@ -1,18 +1,40 @@
-angular.module("activate", ["ui.router"])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider.state("home", {
-            url: "/home",
-            templateUrl: "templates/home.html"
-        })
-            .state("contact", {
-                url: "/contact",
-                template: "<h1>Contact</h1><p>Ui Router Contact</p>"
+(function(){
+    var module = angular.module("activate", ["ui.router"]);
+
+
+    module.config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider.state("home", {
+                url: "/home",
+                templateUrl: "templates/home.html"
             })
-            .state("about", {
-                url: "/about",
-                template: '<h1>About</h1><p>{{name}}</p>',
-                controller : function ($scope) {
-                    $scope.name = "Ui Router About"
-                }
-            })
-    });
+                .state("contact", {
+                    url: "/contact",
+                    template: "<h1>Contact</h1><p>Ui Router Contact</p>"
+                })
+                .state("uistate", {
+                    url: "/uistate",
+                    template: "<h1>State</h1><p>Ui State</p>"
+                })
+                .state("about", {
+                    url: "/about",
+                    template: '<h1>About</h1><p>{{name}}</p>',
+                    controller : function ($scope) {
+                        $scope.name = "Ui Router About"
+                    }
+                })
+        });
+
+
+    module.controller('ActivateCtrl', activateCtrl);
+
+    activateCtrl.$inject = ['$scope', '$state'];
+
+    function activateCtrl(scope, state) {
+        scope.goToAbout = function() {
+            state.go('about');
+        }
+
+        scope.uiState = "uistate";
+
+    }
+})();
